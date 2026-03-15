cff948e49d9235241c296fc2e5932372e84c6493
DHARMESH SHARMA <sharmadharmesh1998@gmail.com>
2026-03-15 13:51:06 +0530

    package json file updated for render

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Budget bee is running!')
-})
+const PORT = process.env.PORT || 8080;
 
-app.listen(8080, () => {
-  console.log('Server is running on http://localhost:8080')
-})
+app.get("/", (req, res) => {
+  res.send("BudgetBee API Running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
