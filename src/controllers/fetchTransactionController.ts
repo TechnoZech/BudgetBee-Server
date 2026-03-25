d1cfedaa1ad7302d41816c89d0713a49b2515f92
DHARMESH SHARMA <sharmadharmesh1998@gmail.com>
2026-03-25 23:03:06 +0530

    total expense and total income added

--- src/controllers/fetchTransactionController.ts
+++ src/controllers/fetchTransactionController.ts
@@ -4,7 +4,10 @@ import Transaction from "../models/Transaction";
 const fetchTransactionController = async (req: any, res: Response) => {
     const userId = req.user._id;
     try {
-        const transactions = await Transaction.find({ user: userId }).sort({ date: -1 });
+        const currentDate = new Date();
+        const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+        const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
+        const transactions = await Transaction.find({ user: userId, date: { $gte: firstDayOfMonth, $lte: lastDayOfMonth } }).sort({ date: -1 });
         return res.status(200).json(transactions);
     } catch (error: any) {
         return res.status(500).json({ message: "Failed to fetch transactions", error: error.message });
