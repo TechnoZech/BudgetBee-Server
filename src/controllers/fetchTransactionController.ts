08a74f53ba9c41a46dc03ce04313587eb56dccab
DHARMESH SHARMA <sharmadharmesh1998@gmail.com>
2026-04-01 12:57:53 +0530

    timezone for transactions adjusted as per the user browser timezone

--- src/controllers/fetchTransactionController.ts
+++ src/controllers/fetchTransactionController.ts
@@ -1,30 +1,33 @@
 import { Request, Response } from "express";
 import Transaction from "../models/Transaction";
+import { fromZonedTime, toZonedTime } from "date-fns-tz";
+import { startOfMonth, endOfMonth } from "date-fns";
 
 const fetchTransactionController = async (req: any, res: Response) => {
 	const userId = req.user._id;
 	try {
-		const currentDate = new Date();
-		const firstDayOfMonth = new Date(
-			currentDate.getFullYear(),
-			currentDate.getMonth(),
-			1,
-		);
-		const lastDayOfMonth = new Date(
-			currentDate.getFullYear(),
-			currentDate.getMonth() + 1,
-			0,
-		);
+		const { timezone } = req.query as { timezone: string };
+		const now = new Date();
+
+		const userNow = toZonedTime(now, timezone);
+		
+
+		const userFirstDay = startOfMonth(userNow); 
+		const userLastDay = endOfMonth(userNow);
+
+		const firstDayUtc = fromZonedTime(userFirstDay, timezone);
+		const lastDayUtc = fromZonedTime(userLastDay, timezone);
+
 		const transactions = await Transaction.find({
 			user: userId,
-			date: { $gte: firstDayOfMonth, $lte: lastDayOfMonth },
+			date: { $gte: firstDayUtc, $lte: lastDayUtc },
 		})
 			.populate("category", "name")
 			.sort({ date: -1 });
-            
+
 		const formattedTransactions = transactions.map((tx) => ({
 			...tx.toObject(),
-			category: (tx as any).category?.name || null, 
+			category: (tx as any).category?.name || null,
 		}));
 		return res.status(200).json(formattedTransactions);
 	} catch (error: any) {
--- src/middleware/protect.ts
+++ src/middleware/protect.ts
@@ -2,39 +2,39 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/User";
 
-export const protect = async (
-  req: any,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const authHeader = req.headers.authorization;
-    const token = authHeader?.startsWith("Bearer ")
-      ? authHeader.split(" ")[1]
-      : null;
+export const protect = async (req: any, res: Response, next: NextFunction) => {
+	try {
+		const authHeader = req.headers.authorization;
+		const token = authHeader?.startsWith("Bearer ")
+			? authHeader.split(" ")[1]
+			: null;
 
-    if (!token) {
-      return res.status(401).json({ message: "Not authorized, token missing" });
-    }
+		if (!token) {
+			console.log("Not authorized, token missing");
+			return res.status(401).json({ message: "Not authorized, token missing" });
+		}
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
-      userId: string;
-      iat: number;
-      exp: number;
-    };
+		const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+			userId: string;
+			iat: number;
+			exp: number;
+		};
 
-    const user = await User.findById(decoded.userId).select("-__v -password");
-    if (!user) {
-      return res.status(401).json({ message: "Not authorized, user not found" });
-    }
+		const user = await User.findById(decoded.userId).select("-__v -password");
+		if (!user) {
+            console.log('user not found')
+			return res
+				.status(401)
+				.json({ message: "Not authorized, user not found" });
+		}
 
-    req.user = user;
+		req.user = user;
 
-    next();
-  } catch (err) {
-    console.error("Protect middleware error:", err);
-    res.status(401).json({ message: "Token invalid" });
-  }
+		next();
+	} catch (err) {
+		console.error("Protect middleware error:", err);
+		res.status(401).json({ message: "Token invalid" });
+	}
 };
 
 export default protect;
